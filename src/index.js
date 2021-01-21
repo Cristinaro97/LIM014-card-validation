b341a03e275cbe1327b383f43ac7362263950a7d
Cristinaro97 <cristinagrl@hotmail.com>
2021-01-21 09:12:00 -0500

    implementacion de la funcion validator

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 import validator from './validator.js';
+ 
+const validar= document.getElementById("validar");
+validar.addEventListener("click",()=>{
+    const tarjeta= document.getElementById("tarjeta").value;
+    let validador = validator.isValid(tarjeta);
 
-console.log(validator);
+    if (validador == true ){
+        alert("Tu tarjeta es válida");
+    } else {
+        alert("Tu tarjeta es inválida");
+    }
+    console.log(validator.isValid(tarjeta));}); 
+
+//Validar datos 
+//getelementbyid para los datos
+//interacciones al botón 
+//elementos
+//selectores
+//tratamiento de strings 
